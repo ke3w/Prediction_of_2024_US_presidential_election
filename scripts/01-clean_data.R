63e3df5e01ab9258d34d3d12858a1bf7f542b992
Kerwin Wu <kerwinwins0221@gmail.com>
2024-11-04 02:10:31 -0500

    clean data change

--- scripts/01-clean_data.R
+++ scripts/01-clean_data.R
@@ -1,40 +1,45 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
+# Purpose: Build a model to predict the winner of the election using cleaned poll data
 # Author: Xinze Wu
 # Date: 2 November 2024
 # Contact: kerwin.wu@utoronto.ca
 # License: MIT
 
 #### Workspace setup ####
+library(readr)
 library(tidyverse)
-library(gt)
 library(arrow)
-
-#### Load data ####
-election_data <- read_csv("data/01-raw_data/president_polls.csv")
+library(janitor)
+library(lubridate)
 
 #### Clean data ####
-# Remove unnecessary columns and handle missing values
-polls_data_cleaned <- election_data %>%
-  dplyr::select(pollscore, sample_size, state, party, candidate_name, pct, end_date) %>%
-  filter(!is.na(pollscore) & !is.na(sample_size) & !is.na(state) & !is.na(pct) & !is.na(end_date))
+clean_president_polls <- read_csv("data/01-raw_data/president_polls.csv") %>%
+  select(candidate_name, state, start_date, end_date, sample_size, numeric_grade) %>%
+  clean_names()
 
-# Create binary 'win' variable indicating if the candidate has the highest percentage in that poll
-polls_data_cleaned <- polls_data_cleaned %>%
-  group_by(state, end_date) %>%
-  mutate(win = ifelse(pct == max(pct), 1, 0),
-         party_binary = ifelse(party == "DEM", 1, 0)) %>%
-  ungroup()
+# Create binary variable for state or national polls and modify state names
+clean_president_polls <- clean_president_polls %>%
+  mutate(
+    state = str_replace_all(state, c("Maine CD-[12]" = "Maine",
+                                     "Nebraska CD-[1-3]" = "Nebraska")),
+    end_date = mdy(end_date),
+    start_date = mdy(start_date),
+    is_harris = ifelse(candidate_name == "Kamala Harris", 1, 0)
+  ) %>%
+  drop_na(sample_size, numeric_grade, state)
 
-# Display cleaned data
-polls_data_cleaned %>%
-  head() %>%
-  gt() %>%
-  tab_header(
-    title = "Cleaned Poll Data",
-    subtitle = "Displaying a few sample of the cleaned data"
-  )
+# Filter for high quality pollsters and state-specific Harris and Trump polls
+clean_president_polls <- clean_president_polls %>%
+  filter(candidate_name %in% c("Kamala Harris", "Donald Trump"),
+         start_date >= ymd("2024-07-21"))
+
+#### Identify, Count, and Drop Duplicates ####
+# Drop duplicates and keep only unique rows
+num_duplicates <- sum(duplicated(clean_president_polls))
+message("Number of duplicate observations: ", num_duplicates)
+
+clean_president_polls <- distinct(clean_president_polls)
+message("Number of rows after removing duplicates: ", nrow(clean_president_polls))
 
 #### Save data ####
-write_csv(polls_data_cleaned, "data/02-analysis_data/cleaned_president_polls.csv")
-write_parquet(polls_data_cleaned, "data/02-analysis_data/cleaned_president_polls.parquet")
+write_parquet(clean_president_polls, "data/02-analysis_data/cleaned_president_polls.parquet")
