4aaacc50fe02165e1c8a5ff796e2dcaae6d1dd78
Kerwin Wu <kerwinwins0221@gmail.com>
2024-11-03 20:53:20 -0500

    1

--- scripts/04-model_data.R
+++ scripts/04-model_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Improved GLM model for predicting election winner
+# Purpose: Improved GLM model for predicting election winner with log transformation
 # Author: Xinze Wu
 # Date: 3 November 2024
 # Contact: kerwin.wu@utoronto.ca
@@ -11,15 +11,20 @@ library(knitr)
 library(car)  # For VIF calculation
 library(pROC)  # For ROC curve and AUC
 
-#### Read data ####
+#### Read Data ####
 model_data <- read_csv("data/02-analysis_data/cleaned_president_polls.csv")
 
+### Transform Data ####
+# Apply log transformation to sample_size
+model_data <- model_data %>%
+  mutate(log_sample_size = log(sample_size))
+
 ### Model Data ####
-# Fit the GLM model using logistic regression
-glm_model <- glm(win ~ pollscore + sample_size + state + party_binary, 
+# Fit the GLM model using logistic regression with log-transformed sample size
+glm_model <- glm(win ~ pollscore + log_sample_size + state + party_binary, 
                  family = binomial, data = model_data)
 
-#### Multicollinearity Check ####F
+#### Multicollinearity Check ####
 vif_values <- vif(glm_model)
 print(vif_values)
 
@@ -33,16 +38,16 @@ glm_tidy <- tidy(glm_model)
 
 # Display the model coefficients
 glm_tidy %>%
-  kable(caption = "Model Coefficients from the GLM Model")
+  kable(caption = "Model Coefficients from the GLM Model with Log-Transformed Sample Size")
 
 # Calculate and plot ROC Curve to assess model performance
 roc_curve <- roc(model_data$win, fitted(glm_model))
-plot(roc_curve, main = "ROC Curve for GLM Model")
+plot(roc_curve, main = "ROC Curve for GLM Model with Log-Transformed Sample Size")
 auc_value <- auc(roc_curve)
 print(paste("AUC Value:", auc_value))
 
 #### Save Model ####
 saveRDS(
   glm_model,
-  file = "models/glm_model.rds"
+  file = "models/glm_model_log_transform.rds"
 )
